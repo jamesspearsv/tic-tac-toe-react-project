5841a644fb083a31c8a8e61596a90887f5b02693
James Spears <jamesspearsv@gmail.com>
2023-07-23 19:55:08 -0400

    Remove extra dist files.

--- src/compnents/GameCard.jsx
+++ src/compnents/GameCard.jsx
@@ -6,10 +6,12 @@ export default function GameCard(props) {
         props.updateBoard(props.index)
     }
 
+    const size = 70
+
     if (props.value == "O") {
         return (
             <td onClick={handleClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="125" height="125" fill="currentColor" className="bi bi-circle" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}  fill="currentColor" className="bi bi-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 </svg>
             </td>
@@ -17,7 +19,7 @@ export default function GameCard(props) {
     } else if (props.value == "X") {
         return (
             <td onClick={handleClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="125" height="125" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width={size}  height={size}  fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                 </svg>
             </td>
